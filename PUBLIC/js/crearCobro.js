7b3a81d35b866c5efbb4cfb12200f74546bbc88d
DAYORITO <107157823+DAYORITO@users.noreply.github.com>
2023-06-26 11:06:13 -0500

    Update crearCobro.js

--- PUBLIC/js/crearCobro.js
+++ PUBLIC/js/crearCobro.js
@@ -5,33 +5,46 @@ const crearCobro = async () =>{
     let valor = document.getElementById('valorC').value;
     let descripcion = document.getElementById('descripcionC').value;
 
-    let cobro = {
-        codigoCobro: codigoCobro,
-        tipoCobro: tipoCobro,
-        valor: valor,
-        descripcion: descripcion
-    }
-    console.log(cobro)
-        fetch(URL, {
-            method: 'POST',
-            mode: 'cors',
-            body:JSON.stringify(cobro),
-            headers: {"Content-type": "application/json; charset=UTF-8"}     
+    try{
+        if(!regexCodigo.test(codigoCobro) && !codigoCobro && !Regextipo.test(tipoCobro) && !tipoCobro && !descripcion && !valor){
+            throw new Error('Los campos no son validos o estna vacios');
+        }
+        let cobro = {
+            codigoCobro: codigoCobro,
+            tipoCobro: tipoCobro,
+            valor: valor,
+            descripcion: descripcion
+        }
+        console.log(cobro)
+            fetch(URL, {
+                method: 'POST',
+                mode: 'cors',
+                body:JSON.stringify(cobro),
+                headers: {"Content-type": "application/json; charset=UTF-8"}     
+            })
+            .then(response => response.json()) //La respuesta del método POST de la API
+            .then(data => {
+                console.log(data);
+                Swal.fire(
+                    'Muy bien!',
+                    'Cuenta creada exitosamente',
+                    'success'
+                  ).then(function(result) {
+                    if (result.isConfirmed) {
+                      // Redireccionar a una página específica cuando se hace clic en "Aceptar"
+                      window.location.href = "cobros";
+                    }
+                  })
         })
-        .then(response => response.json()) //La respuesta del método POST de la API
-        .then(data => {
-            console.log(data);
-            Swal.fire(
-                'Muy bien!',
-                'Cuenta creada exitosamente',
-                'success'
-              ).then(function(result) {
-                if (result.isConfirmed) {
-                  // Redireccionar a una página específica cuando se hace clic en "Aceptar"
-                  window.location.href = "cobros";
-                }
-              })
-    })
+    }catch(e){
+        Swal.fire(
+            'Error',
+            e.message,
+            'error'
+        )
+
+    }
+
 }
 document.getElementById('createForm').addEventListener("submit", function(e){
     e.preventDefault();
